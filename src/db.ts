01b636873ead655075d5916cb583e35af0ad961a
lopez-jakim <jakimdlopez@iskolarngbayan.pup.edu.ph>
2025-12-26 19:05:43 +0800

    chore: update database name

--- src/db.ts
+++ src/db.ts
@@ -3,8 +3,8 @@ import mysql from 'mysql2/promise';
 export const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'accreditrack_db_2',
+  password: process.env.DB_PASSWORD || '1028',
+  database: process.env.DB_NAME || 'accreditrack_db',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
